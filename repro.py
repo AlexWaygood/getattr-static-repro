bbb9c84132a8beed6656e8c2a66471ebee77e10e
AlexWaygood <alex.waygood@gmail.com>
2023-05-31 12:12:47 +0100

    Delete some stuff

--- repro.py
+++ repro.py
@@ -12,20 +12,6 @@ class TrackableDataStructure:
     self._self_trainable = True
     self._self_extra_variables = []
 
-  def _track_value(self, value, name):
-    value = sticky_attribute_assignment(
-        trackable=self, value=value, name=name)
-    if isinstance(value, variables.Variable):
-      self._self_extra_variables.append(value)
-    if not isinstance(value, base.Trackable):
-      raise _UntrackableError(value)
-    if hasattr(value, "_use_resource_variables"):
-      value._use_resource_variables = True  # pylint: disable=protected-access
-    value_attribute_sentinel = getattr(value, "_attribute_sentinel", None)
-    if value_attribute_sentinel:
-      value_attribute_sentinel.add_parent(self._attribute_sentinel)
-    return value
-
   @property
   def _values(self):
     """An iterable/sequence which may contain trackable objects."""
