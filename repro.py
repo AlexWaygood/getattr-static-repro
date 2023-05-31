70255399f5551699337a1d1e55a62ed7218211d8
AlexWaygood <alex.waygood@gmail.com>
2023-05-31 12:14:02 +0100

    Delete some stuff

--- repro.py
+++ repro.py
@@ -3,14 +3,7 @@ import inspect
 
 import wrapt
 
-class TrackableDataStructure:
-  def __init__(self):
-    # Attributes prefixed with "_self_" for compatibility with
-    # wrapt.ObjectProxy. All additional attrs MUST conform to this pattern, as
-    # extending `__slots__` on a subclass of ObjectProxy breaks in a variety of
-    # ways.
-    self._self_trainable = True
-    self._self_extra_variables = []
+class TrackableDataStructure: pass
 
 class _DictWrapper(TrackableDataStructure, wrapt.ObjectProxy):
   """Wraps built-in dicts to support restore-on-create for variables.
