a0f7bdb6f7d5dd98be035de6f79699a9c7dd543f
AlexWaygood <alex.waygood@gmail.com>
2023-05-31 12:15:27 +0100

    Delete some stuff

--- repro.py
+++ repro.py
@@ -42,46 +42,6 @@ class _DictWrapper(TrackableDataStructure, wrapt.ObjectProxy):
       return super().__getattribute__(name)
 
   @property
-  def _values(self):
-    """Collect values for TrackableDataStructure."""
-    # Sort items deterministically by key
-    ordered = list(zip(*sorted(self.items(), key=lambda it: it[0])))
-    if ordered:
-      return ordered[1]
-    return []
-
-  def _trackable_children(self, save_type=..., **kwargs):
-    """Check that the object is saveable before listing its dependencies."""
-    self._check_self_external_modification()
-    if self._self_non_string_key:
-      raise ValueError(
-          f"Unable to save the object {self} (a dictionary wrapper constructed "
-          "automatically on attribute assignment). The wrapped dictionary "
-          "contains a non-string key which maps to a trackable object or "
-          "mutable data structure.\n\nIf you don't need this dictionary "
-          "checkpointed, wrap it in a non-trackable "
-          "object; it will be subsequently ignored.")
-    if self._self_external_modification:
-      raise ValueError(
-          f"Unable to save the object {self} (a dictionary wrapper constructed "
-          "automatically on attribute assignment). The wrapped dictionary was "
-          f"modified outside the wrapper (its final value was {self}, its value"
-          " when a checkpoint dependency was added was "
-          f"{self._self_last_wrapped_dict_snapshot}), which breaks "
-          "restoration on object creation.\n\nIf you don't need this "
-          "dictionary checkpointed, wrap it in a "
-          "non-trackable object; it will be subsequently ignored.")
-    assert not self._dirty  # Any reason for dirtiness should have an exception.
-    children = super()._trackable_children(save_type, **kwargs)
-
-    if save_type == base.SaveType.SAVEDMODEL:
-      # Add functions to be serialized.
-      children.update(
-          {key: value for key, value in self.items() if _is_function(value)})
-
-    return children
-
-  @property
   def _dirty(self):
     """Check if there has already been a mutation which prevents saving."""
     return (self._self_external_modification
