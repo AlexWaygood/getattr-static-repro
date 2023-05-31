982748a2c0d86cacb79e8a8eae3525164504b906
AlexWaygood <alex.waygood@gmail.com>
2023-05-31 12:16:08 +0100

    Delete some stuff

--- repro.py
+++ repro.py
@@ -47,28 +47,5 @@ class _DictWrapper(TrackableDataStructure, wrapt.ObjectProxy):
     return (self._self_external_modification
             or self._self_non_string_key)
 
-  def __setitem__(self, key, value):
-    """Allow any modifications, but possibly mark the wrapper as unsaveable."""
-    self._check_self_external_modification()
-    self._maybe_initialize_trackable()
-    no_dep = isinstance(value, NoDependency)
-    if isinstance(key, str):
-      value = self._track_value(value, name=key)
-    else:
-      value = wrap_or_unwrap(value)
-      if not no_dep and isinstance(value, base.Trackable):
-        # Non-string keys are OK as long as we have no reason to add a
-        # dependency on the value (either because the value is not
-        # trackable, or because it was wrapped in a NoDependency object).
-        self._self_non_string_key = True
-    self.__wrapped__[key] = value
-
-    self._update_snapshot()
-
-  def __delitem__(self, key):
-    self._check_self_external_modification()
-    del self.__wrapped__[key]
-    self._update_snapshot()
-
 
 inspect.getattr_static(_DictWrapper(), 'bar')
